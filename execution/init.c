a7182930e20936766de3c57dda9cc8f6c32d99eb
Your Name <you@example.com>
2025-06-12 00:00:35 +0000

    init

--- execution/init.c
+++ execution/init.c
@@ -4,11 +4,11 @@ void	failure_detect(t_status status)
 {
 	if (!status.fail)
 		return ;
-	if (status.sucesses-- != 0)
+	if (status.sucesses-- > 0)
 		mlx_destroy_image(status.vars->mlx, status.vars->img.ptr);
-	if (status.sucesses-- != 0)
+	if (status.sucesses-- > 0)
 		mlx_destroy_window(status.vars->mlx, status.vars->win);
-	if (status.sucesses-- != 0)
+	if (status.sucesses-- > 0)
 		mlx_destroy_display(status.vars->mlx);
 		// free
 }
