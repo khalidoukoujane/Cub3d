6b74695bcda1268f6b0d99bb461dbd1e224b99aa
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-06-12 10:06:56 +0100

    aadded

---
+++ parser/compile.sh
@@ -0,0 +1,2 @@
+
+cc *.c ../gnl/*.c ../general_utils/*.c -o Cub3d
---
+++ parser/mini_main.c
@@ -0,0 +1,20 @@
+# include "../cub.h"
+
+int	main(int ac, char **av)
+{
+	t_vars  vars;
+
+	vars.data = malloc(sizeof(t_parsed));
+	if (!vars.data)
+		return (ft_error("faild to allocate"), 1);
+	if (!ft_parser(ac, av, &vars.data))
+		return (1);
+	// init
+	// init_program(&vars);
+	// render
+	// render(&vars);
+	// hook
+	// mlx_loop(vars.mlx);
+	// cleanup
+	return (EXIT_SUCCESS);
+}
