eee846a1141915a552038a874aa6eed42b93ff6e
Your Name <you@example.com>
2025-06-14 15:15:52 +0000

    texturing

--- execution/render.c
+++ execution/render.c
@@ -34,87 +34,97 @@
 // }
 
 
-
 #include "../cub.h"
 #include <math.h>
 
 #define FOV 0.66
 
-int	get_wall_color(int side, int y, int wall_height, int tex_width, int tex_height, int *wall_data)
+int	get_wall_color(int side, int y, int wall_height, int tex_x, int tex_width, int tex_height, int *wall_data)
 {
-	int tex_y;
-	int color;
-	
+	int tex_y = (y * tex_height) / wall_height;
 
-	// Map screen y to texture y
-	tex_y = (y * tex_height) / wall_height;
-
-	// Clamp to texture height
 	if (tex_y < 0) tex_y = 0;
 	if (tex_y >= tex_height) tex_y = tex_height - 1;
 
-	// Sample vertical column (assuming 1D texture for simplification)
-	color = wall_data[tex_y * tex_width];  // assuming column x = 0
+	int color = wall_data[tex_y * tex_width + tex_x];
 
 	if (side == 1)
-		color = (color >> 1) & 0x7F7F7F; // darken for shading
+		color = (color >> 1) & 0x7F7F7F;
 
 	return color;
 }
 
-void draw_line(t_vars *vars, int x, t_ray ray, int tex_width, int tex_height, int *wall_data)
+void draw_line(t_vars *vars, int x, t_ray ray, int tex_x, int tex_width, int tex_height, int *wall_data)
 {
-    int start;
-    int end;
-    int length;
-    int color;
+	int start, end, length, color;
 
-    length = HEIGHT / ray.distance;
-    if (length > HEIGHT)
-        length = HEIGHT;
-    start = (HEIGHT - length) / 2;
-    end = start + length;
+	if (ray.distance < 0.0001)
+		ray.distance = 0.0001;
 
+	length = HEIGHT / ray.distance;
+	if (length > HEIGHT)
+		length = HEIGHT;
 
+	start = (HEIGHT - length) / 2;
+	end = start + length;
 
-    while (start < end)
-    {
-        color = get_wall_color(ray.side, start, length, tex_width, tex_height, wall_data);
-        mlx_pixel_put(vars->mlx, vars->win, x, start, color);
-        start++;
-    }
+	if (start < 0) start = 0;
+	if (end > HEIGHT) end = HEIGHT;
+
+	while (start < end)
+	{
+		color = get_wall_color(ray.side, start, length, tex_x, tex_width, tex_height, wall_data);
+		mlx_pixel_put(vars->mlx, vars->win, x, start, color);
+		start++;
+	}
 }
 
 void render(t_vars *vars)
 {
-    t_ray ray;
-    int x;
+	t_ray ray;
+	int x = 0;
 
-
-
-
-	int			*wall_data;
-	void		*wall_img;
-	int			bpp, size_line, endian;
-	int			tex_width, tex_height;
+	int *wall_data;
+	void *wall_img;
+	int bpp, size_line, endian;
+	int tex_width, tex_height;
 
 	wall_img = mlx_xpm_file_to_image(vars->mlx, "wall.xpm", &tex_width, &tex_height);
-	if (wall_img == NULL)
-		exit(1);
+	if (!wall_img)
+	{
+		perror("Failed to load wall texture");
+		exit(EXIT_FAILURE);
+	}
+
 	wall_data = (int *)mlx_get_data_addr(wall_img, &bpp, &size_line, &endian);
 
-
-
-    x = 0;
 	mlx_clear_window(vars->mlx, vars->win);
-    while (x < WIDTH)
-    {
-        double camera_x = 2 * x / (double)WIDTH - 1; // value from -1 to 1
-        double ray_angle = vars->player.angle + atan(camera_x * FOV); // perspective correction
-        ray_init(&ray, vars->player.position, ray_angle);
-        ray_cast(vars, &ray);
-        draw_line(vars, x, ray, tex_width, tex_height, wall_data);
-        x++;
-    }
+
+	while (x < WIDTH)
+	{
+		double camera_x = 2 * x / (double)WIDTH - 1;
+		double ray_angle = vars->player.angle + atan(camera_x * FOV);
+
+		ray_init(&ray, vars->player.position, ray_angle);
+		ray_cast(vars, &ray);
+
+		// Compute texture X coordinate
+		double wall_x;
+		if (ray.side == 0)
+			wall_x = ray.origin.y + ray.distance * ray.direction.y;
+		else
+			wall_x = ray.origin.x + ray.distance * ray.direction.x;
+
+		wall_x -= floor(wall_x); // keep only the fractional part
+		int tex_x = (int)(wall_x * tex_width);
+
+		// Flip tex_x if needed to avoid mirrored textures
+		if ((ray.side == 0 && ray.direction.x > 0) || (ray.side == 1 && ray.direction.y < 0))
+			tex_x = tex_width - tex_x - 1;
+
+		draw_line(vars, x, ray, tex_x, tex_width, tex_height, wall_data);
+		x++;
+	}
 }
+
 
--- linux_compile.bash
+++ linux_compile.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 gcc *.c execution/*.c parser/*.c gnl/*.c general_utils/*.c -o minicub3d \
-  -g -fsanitize=address \
   -I minilibx-linux \
   -L minilibx-linux \
   -lmlx -lXext -lX11 -lm
+
+  # -g -fsanitize=address \
