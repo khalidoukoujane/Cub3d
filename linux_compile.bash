5856b98ecc6ac3043978e5e74a376bf285e19948
Issa Oulkhir <ioulkhir@e2r2p1.1337.ma>
2025-06-21 07:08:16 +0100

    basic setup

--- linux_compile.bash
+++ linux_compile.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 gcc *.c execution/*.c parser/*.c gnl/*.c general_utils/*.c -o minicub3d \
-  -I minilibx-linux \
-  -L minilibx-linux \
+  -I mlx \
+  -L mlx \
   -lmlx -lXext -lX11 -lm
 
   # -g -fsanitize=address \
