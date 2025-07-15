8c54664786e861ef138dac58e3ad10643cc808e5
Khalid  Oukoujane <khoukouj@e3r9p1.1337.ma>
2025-07-15 16:20:33 +0100

    added

--- animation/animation.c
+++ animation/animation.c
@@ -6,7 +6,7 @@
 /*   By: khoukouj <khoukouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 09:53:30 by khoukouj          #+#    #+#             */
-/*   Updated: 2025/07/15 16:04:01 by khoukouj         ###   ########.fr       */
+/*   Updated: 2025/07/15 16:18:25 by khoukouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	scale_frame_to_window(t_vars *v, t_img *frame, t_img *s_frame)
 			it.src_h = (int)(it.i * it.s_y);
 			it.color = ft_get_color(frame, it.src_w, it.src_h);
 			my_mlx_pixel_put(s_frame, it.j, it.i, it.color);
-			it.j++;	
+			it.j++;
 		}
 		it.i++;
 	}
@@ -52,6 +52,7 @@ void	animation(t_vars *v)
 		v->anim.frame_timer = 0;
 		v->anim.curr_frame = (v->anim.curr_frame + 1) % v->anim.frame_counter;
 	}
-	scale_frame_to_window(v, &v->anim.frames[v->anim.curr_frame], &v->anim.scaled_frm);
+	scale_frame_to_window(v, &v->anim.frames[v->anim.curr_frame], \
+		&v->anim.scaled_frm);
 	mlx_put_image_to_window(v->mlx, v->win, v->anim.scaled_frm.ptr, 0, 0);
 }
--- cub.h
+++ cub.h
@@ -6,7 +6,7 @@
 /*   By: khoukouj <khoukouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 12:59:53 by khoukouj          #+#    #+#             */
-/*   Updated: 2025/07/15 12:24:25 by khoukouj         ###   ########.fr       */
+/*   Updated: 2025/07/15 16:20:11 by khoukouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,61 +51,62 @@
 void			ft_load_frames(t_vars *v);
 unsigned int	ft_get_color(t_img *img, int x, int y);
 void			setup_scaled_frame(t_vars *v);
-void	setup_animation(t_vars *v);
-void	animation(t_vars *v);
-void	ft_error(char *arg);
-void	init_data(t_parsed **data);
-void	free_data(t_vars *ptr);
-void	ft_close_fds(int *fds);
-int		do_textures_checks(t_parsed **data);
-int		copy_map(t_parsed **data, char **content, int i);
-int		get_player_pos(t_parsed **data);
-int		do_conf_checks(t_parsed **data);
-int		is_match_of(char c, char *line);
-int		ft_parser(int ac, char **av, t_parsed **data);
-char	*get_next_line(int fd);
-int		check_config(char **content);
-int		check_map(char **content, int start);
-int		is_player(char c);
-int		valid_line(char *line);
-int		is_surrounded(char **content, int i);
-int		is_config_line(char *line);
-int		is_color_line(char *line);
-int		is_texture_line(char *line);
-int		is_whitespace(char c);
-int		is_only_whitespace(char *str);
-void	skip_newlines(char **content, int *i);
-int		grep_data(t_parsed **data);
-char	**ft_spliter(char *str, char *charset);
-void	fill_colors(char **data, t_color *color);
-int		free_splited(char **ss);
+void			setup_animation(t_vars *v);
+void			animation(t_vars *v);
+void			ft_error(char *arg);
+void			init_data(t_parsed **data);
+void			free_data(t_vars *ptr);
+void			ft_close_fds(int *fds);
+int				do_textures_checks(t_parsed **data);
+int				copy_map(t_parsed **data, char **content, int i);
+int				get_player_pos(t_parsed **data);
+int				do_conf_checks(t_parsed **data);
+int				is_match_of(char c, char *line);
+int				ft_parser(int ac, char **av, t_parsed **data);
+char			*get_next_line(int fd);
+int				check_config(char **content);
+int				check_map(char **content, int start);
+int				is_player(char c);
+int				valid_line(char *line);
+int				is_surrounded(char **content, int i);
+int				is_config_line(char *line);
+int				is_color_line(char *line);
+int				is_texture_line(char *line);
+int				is_whitespace(char c);
+int				is_only_whitespace(char *str);
+void			skip_newlines(char **content, int *i);
+int				grep_data(t_parsed **data);
+char			**ft_spliter(char *str, char *charset);
+void			fill_colors(char **data, t_color *color);
+int				free_splited(char **ss);
 
 // init
-void	failure_detect(t_status status);
-void	init_program(t_vars *vars);
+void			failure_detect(t_status status);
+void			init_program(t_vars *vars);
 
 // wrappers
-void	*ft_mlx_init(t_status *status);
-void	*ft_mlx_new_window(t_status *status);
-void	*ft_mlx_new_image(t_status *status);
-void	*ft_mlx_xpm_file_to_image(t_status *status, char *filename, t_tex *tex);
-void	my_mlx_pixel_put(t_img *data, int x, int y, int color);
+void			*ft_mlx_init(t_status *status);
+void			*ft_mlx_new_window(t_status *status);
+void			*ft_mlx_new_image(t_status *status);
+void			*ft_mlx_xpm_file_to_image(t_status *status,
+					char *filename, t_tex *tex);
+void			my_mlx_pixel_put(t_img *data, int x, int y, int color);
 
 // small utils
 
-double	decimal_part(double num);
-double	zero_protect(double x);
+double			decimal_part(double num);
+double			zero_protect(double x);
 
 // ray casting
-void	ray_init(t_ray *ray, t_vector origin, double theta);
-void	ray_cast(t_vars *vars, t_ray *ray);
-double	get_side_vec(t_ray *ray, t_vector vec);
-void	render(t_vars *vars);
-int		collision(t_vars *vars);
+void			ray_init(t_ray *ray, t_vector origin, double theta);
+void			ray_cast(t_vars *vars, t_ray *ray);
+double			get_side_vec(t_ray *ray, t_vector vec);
+void			render(t_vars *vars);
+int				collision(t_vars *vars);
 
 // equations bro !!
-t_equ	linear_equ(t_ray ray);
-double	solve_equs(t_equ equ, t_ray ray);
+t_equ			linear_equ(t_ray ray);
+double			solve_equs(t_equ equ, t_ray ray);
 
 // events
 // #define ESC   65307   // Escape key
@@ -132,15 +133,15 @@ double	solve_equs(t_equ equ, t_ray ray);
 # define ON_MOUSEMOVE 6
 # define ON_DESTROY	17
 
-int		handle_mouse_down(int button, int x, int y, void *param);
-int		handle_mouse_up(int button, int x, int y, void *param);
-int		handle_mouse_move(int x, int y, void *param);
-int		handle_close(t_vars *vars);
+int				handle_mouse_down(int button, int x, int y, void *param);
+int				handle_mouse_up(int button, int x, int y, void *param);
+int				handle_mouse_move(int x, int y, void *param);
+int				handle_close(t_vars *vars);
 
 // smooth criminal 
-int		key_press(int keycode, t_vars *vars);
-int		key_release(int keycode, t_vars *vars);
-int		game_loop(t_vars *vars);
-int		get_minimap_color(t_vars *vars, int x, int y);
+int				key_press(int keycode, t_vars *vars);
+int				key_release(int keycode, t_vars *vars);
+int				game_loop(t_vars *vars);
+int				get_minimap_color(t_vars *vars, int x, int y);
 
 #endif
