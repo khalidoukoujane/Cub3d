93c14527e7da2b2eba796f8787a76eb2e6c4cff4
Khalid  Oukoujane <khoukouj@e3r8p1.1337.ma>
2025-07-20 10:33:40 +0100

    added

--- parser/grep_file_data.c
+++ parser/grep_file_data.c
@@ -6,7 +6,7 @@
 /*   By: khoukouj <khoukouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 13:54:47 by khoukouj          #+#    #+#             */
-/*   Updated: 2025/07/20 08:45:30 by khoukouj         ###   ########.fr       */
+/*   Updated: 2025/07/20 10:11:03 by khoukouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,11 @@ int	put_data(t_parsed **info, char *line, int *i)
 	char	**data;
 
 	if (is_texture_line(line))
-		data = ft_spliter(line, " \t\n");
+		data = handle_text_part(line);
 	else
 		data = handle_clr_part(line);
 	if (!data || !*data)
-		return (ft_error("invalid colors format"), -1);
+		return (ft_error("invalid format"), -1);
 	if (is_match_of('T', data[0]) && count_splited(data) != 2)
 		return (ft_error("invalid textures"), -1);
 	else if (is_match_of('C', data[0]) && count_splited(data) != 4)
--- parser/utils.c
+++ parser/utils.c
@@ -6,7 +6,7 @@
 /*   By: khoukouj <khoukouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 12:51:38 by khoukouj          #+#    #+#             */
-/*   Updated: 2025/07/20 08:46:55 by khoukouj         ###   ########.fr       */
+/*   Updated: 2025/07/20 10:26:35 by khoukouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,4 +85,54 @@ char	**handle_clr_part(char *line)
 	}
 	res[count_splited(clrs) + 1] = NULL;
 	return (free_splited(data), free_splited(clrs), res);
+}
+
+char	*ft_grep_path(char *line)
+{
+	char	*path_start;
+	char	*path;
+	int		len;
+	int		i;
+
+	path_start = line;
+	while (*path_start && !(*path_start == ' ' || *path_start == '\t'))
+		path_start++;
+	while (*path_start && (*path_start == ' ' || *path_start == '\t'))
+		path_start++;
+	len = ft_strlen(path_start);
+	if (len > 0 && path_start[len - 1] == '\n')
+		len--;
+	path = malloc(len + 1);
+	if (!path)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		path[i] = path_start[i];
+		i++;
+	}
+	return (path[len] = '\0', path);
+}
+
+char	**handle_text_part(char *line)
+{
+	char	**res;
+	char	**data;
+	char	*path;
+
+	path = NULL;
+	data = ft_spliter(line, " \t\n");
+	if (!data)
+		return (NULL);
+	path = ft_grep_path(line);
+	printf("%s\n", path);
+	if (!path)
+		return (free_splited(data), NULL);
+	res = malloc(sizeof(char *) * 3);
+	if (!res)
+		return (free_splited(data), free(path), NULL);
+	res[0] = ft_strdup(data[0]);
+	res[1] = path;
+	res[2] = NULL;
+	return (free_splited(data), res);
 }
